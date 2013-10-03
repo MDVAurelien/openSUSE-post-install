baff4cf1084d18d803c9361319ad6de33cda6161
Aurélien Sallé <android.aurelien.salle@gmail.com>
2013-10-03 21:03:07 +0200

    Create openSUSE-post-install-13.1.sh

---
+++ openSUSE-post-install-13.1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Description:
+# A post-installation bash script for openSUSE 13.1
+#
+# Authors : MDVAurelien (Aurélien Sallé) <android.aurelien.salle@gmail.com>
+#
+# openSUSE-post-install is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# openSUSE_post_install is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+RELEASE='13.1' # Do not modify this variable, it's use by repositories URL
+VERSION='0.0.0' # It's the version of this file
+LICENSE='LGPLv3'
+
+
+
+coming soon
